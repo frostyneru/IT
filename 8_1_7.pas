534149e39f041ea33450579086aa2bf0b45a84c6
frostyneru <neru@protonmail.com>
2017-03-11 20:27:10 +0600

    Create 8_1_7.pas

---
+++ 8_1_7.pas
@@ -0,0 +1,8 @@
+var
+     let,let1: set of char;
+     i:byte;
+begin
+     let:=['a','b','c','d','e','f','g','h','1','2','3','4','5','8','9','+','-','*'];
+     let1:=['a'..'z','A'..'Z',',','.'];   
+     writeln(let*let1);          
+end.
